df03c6eaa30669972fd9bf6dbfab28201f73b580
SatoShun <shun.sato1@gmail.com>
2020-11-11 01:41:06 +0000

    format

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,19 @@
 buildscript {
-    repositories {
-        gradlePluginPortal()
-        jcenter()
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.4.10")
-        classpath("com.android.tools.build:gradle:4.1.1")
-    }
+  repositories {
+    gradlePluginPortal()
+    jcenter()
+    google()
+    mavenCentral()
+  }
+  dependencies {
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
+    classpath("org.jetbrains.kotlin:kotlin-serialization:1.4.10")
+    classpath("com.android.tools.build:gradle:4.1.1")
+  }
 }
 group = "com.example.kmmtemplate"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
