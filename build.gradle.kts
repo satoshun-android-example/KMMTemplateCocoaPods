abec7dd72445e428513b08b2e15ef93cc9ecea5f
SatoShun <shun.sato1@gmail.com>
2020-11-11 00:35:33 +0000

    update libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.4.10")
-        classpath("com.android.tools.build:gradle:4.1.0")
+        classpath("com.android.tools.build:gradle:4.1.1")
     }
 }
 group = "com.example.kmmtemplate"
