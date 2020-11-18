a0d35c57d13c0dcb5648a9f94ee2dce8304f70d4
SatoShun <shun.sato1@gmail.com>
2020-11-18 09:35:53 +0000

    pod

--- cocoapodsshared/src/commonMain/kotlin/com/example/kmmtemplate/shared/Greeting.kt
+++ cocoapodsshared/src/commonMain/kotlin/com/example/kmmtemplate/shared/Greeting.kt
@@ -3,6 +3,6 @@ package com.example.kmmtemplate.shared
 
 class Greeting {
     fun greeting(): String {
-        return "Hello, ${Platform().platform}!"
+        return "HelloD, ${Platform().platform}!"
     }
 }
