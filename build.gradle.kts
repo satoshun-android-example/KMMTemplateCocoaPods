d19b170ea5b57c454ecb760ce5faf04bd6397ca4
SatoShun <shun.sato1@gmail.com>
2020-12-01 07:20:40 +0000

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
     mavenCentral()
   }
   dependencies {
-    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
-    classpath("org.jetbrains.kotlin:kotlin-serialization:1.4.10")
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.20")
+    classpath("org.jetbrains.kotlin:kotlin-serialization:1.4.20")
     classpath("com.android.tools.build:gradle:4.1.1")
     classpath("co.touchlab:kotlinnativecocoapods:0.11")
   }
