ff5dba23736b963141ef2a164b428b9b3ebee6bc
SatoShun <shun.sato1@gmail.com>
2020-12-09 05:25:04 +0000

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,10 +6,11 @@ buildscript {
     mavenCentral()
   }
   dependencies {
-    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.20")
-    classpath("org.jetbrains.kotlin:kotlin-serialization:1.4.20")
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
+    classpath("org.jetbrains.kotlin:kotlin-serialization:1.4.21")
     classpath("com.android.tools.build:gradle:4.1.1")
     classpath("co.touchlab:kotlinnativecocoapods:0.11")
+    classpath("com.vanniktech:gradle-maven-publish-plugin:0.11.1")
   }
 }
 group = "com.example.kmmtemplate"
