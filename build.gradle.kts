329a91c68297e587da700fd329d71c863cd2b47a
SatoShun <shun.sato1@gmail.com>
2021-01-25 04:59:17 +0000

    update agp

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
   dependencies {
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
     classpath("org.jetbrains.kotlin:kotlin-serialization:1.4.21")
-    classpath("com.android.tools.build:gradle:4.1.1")
+    classpath("com.android.tools.build:gradle:4.1.2")
     classpath("co.touchlab:kotlinnativecocoapods:0.11")
     classpath("com.vanniktech:gradle-maven-publish-plugin:0.11.1")
   }
