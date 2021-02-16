59a91d26073850c9ef9538899da878bb15bb5b27
SatoShun <shun.sato1@gmail.com>
2021-02-16 04:59:48 +0000

    kt 1.4.30

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
     mavenCentral()
   }
   dependencies {
-    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
-    classpath("org.jetbrains.kotlin:kotlin-serialization:1.4.21")
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
+    classpath("org.jetbrains.kotlin:kotlin-serialization:1.4.30")
     classpath("com.android.tools.build:gradle:4.1.2")
     classpath("com.vanniktech:gradle-maven-publish-plugin:0.11.1")
   }
